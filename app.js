b404d3e0434870a5f52ef43395668f4173691604
iamsixwater <soup.sixwater@gmail.com>
2022-06-19 23:08:12 +0900

    connect DB

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
+require('dotenv').config();
+
+// connect mongodb
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DB_HOST, { useNewUrlParser: true });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
